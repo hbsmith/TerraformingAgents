455c9f9d4f76f6e2f93534c4bc99789d123f0bd9
hbsmith <hbs@asu.edu>
2021-10-09 16:33:10 +0900

    len to length

--- src/TerraformingAgents.jl
+++ src/TerraformingAgents.jl
@@ -108,8 +108,8 @@ mutable struct GalaxyParameters
             throw(ArgumentError("keyword arguments :pos and :vel must have the same length as the width of :planetcompositions"))
         end
 
-        if ~all(x->len(x)==compsize, eachcol(planetcompositions)))
-            throw(ArgumentError("All planets must have `compsize` for their compositions"))
+        if ~all(x->length(x)==compsize, eachcol(planetcompositions)))
+            throw(ArgumentError("All planets compositions must have length of `compsize`"))
         end
         
         ## ABMkwargs
